47a430578fddd735a9030eeea9e74d094070667f
Nikita Shkuratov <nikita.shkuratov@mail.com>
2021-02-10 16:53:49 +0300

    creation of the basic structure

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,9 @@
+import { DomListener } from "./DomListener";
+
+import { DomListener } from "./DomListener";
+
+export class ExcelComponent extends DomListener {
+  toHTML() {
+    return "";
+  }
+}
